57213d21e5729640cc889fc62c20266e5fc718f2
Megan Ekse <meganekse@gmail.com>
2021-10-13 20:28:42 -0700

    folder structure

--- client/src/pages/Contactus.js
+++ client/src/pages/Contactus.js
@@ -4,6 +4,37 @@ const Contact = () => {
     return (
         <div style={{display: "flex", justifyContent: "center"}}>
             <h1>Contact Us</h1>
+            <form>
+        <input
+          value={fullName}
+          name="fullName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <label>
+            Comments:
+            <textarea
+            value={comments}
+          name="comments"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Comments"
+            />
+        </label>
+          
+        
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
         </div>
         );
     
