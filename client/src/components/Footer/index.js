0279e9896854bd1a1879e6436e9f20d5f3c787fa
MaryVPie <pirogova.mariia@gmail.com>
2021-10-21 02:06:57 -0700

    rewritten footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,23 +2,11 @@ import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import { Footing } from "./footerelement.js"
 const Footer = () => {
-  const location = useLocation();
-  const history = useHistory();
   return (
     <footer>
-
-      {location.pathname !== '/' && (
-        <button
-          className="btn btn-dark mb-3"
-          onClick={() => history.goBack()}
-        >
-          &larr; Go Back
-        </button>
-      )}
       <Footing >
         <p >Copyright © 2021  State of Washington  All rights reserved. </p>
       </Footing>
-
     </footer>
   );
 };
