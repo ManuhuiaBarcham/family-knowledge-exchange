5c9b403915ca351ff1d7bcc8ffd53719cc8deb5c
MaryVPie <pirogova.mariia@gmail.com>
2021-10-21 02:08:56 -0700

    rewritten profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,36 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-// import ThoughtForm from '../components/ThoughtForm';
-// import ThoughtList from '../components/ThoughtList';
+import AwesomeSelect from '../components/AwesomeSelect';
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Signup from '../pages/Signup';
-
-import Auth from '../utils/auth';
+import { QUERY_USER, QUERY_USERS, QUERY_ME, QUERY_INTERESTS } from '../utils/queries';
+import UserCard from '../components/UserCard';
+import './Profile.css';
 
 const Profile = () => {
-  const { username: userParam } = useParams();
+  const [selectedInterest, setSelectedInterest] = useState('');
 
-  console.log('ID', userParam);
+  let matchedUsers = [];
+  const handleInterestChange = (event) => {
+    const { value } = event.target;
+    setSelectedInterest(value);
 
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
+  };
+
+  const routeData = useParams();
+  const { username: userParam } = routeData;
+
+  const queryRes = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam }
   });
 
+  const { loading, data } = queryRes
   const user = data?.me || data?.user || {};
   // redirect to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Redirect to="/me" />;
+  // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  //   return <Redirect to="/me" />;
+  // }
+  const interestQueryReply = useQuery(QUERY_INTERESTS);
+  let interests = interestQueryReply.data?.interests || [];
+  if (interestQueryReply.data) {
+    interests = interests.map(interest => {
+      let newInterest = {
+        id: interest._id,
+        name: interest.interestOption
+      }
+      return newInterest;
+    });
   }
+
+  const matchedUsersQueryResults = useQuery(QUERY_USERS, {
+    variables: {
+      filter: {
+        interest: selectedInterest
+      }
+    }
+  });
+  matchedUsers = matchedUsersQueryResults.data?.users || [];
+  console.log(matchedUsers)
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  console.log('ID', data);
-  console.log('ID', user);
 
   if (!user?.username) {
     return (
@@ -40,61 +66,68 @@ const Profile = () => {
       </h4>
     );
   }
-  console.log(data);
+
+
+
   return (
-    <main>
-      <div>
-        <div>
-          {/* the search bar */}
-
-          {/* <button onclick={goToSearchPage}>
-                    Find People with Similar Interests
-                </button> */}
-          <button
-            to="/search"
-            type="button"
-            classNameName="btn btn-primary btn-lg"
-          >
-            Search
-          </button>
+    <>
+      <div className="row">
+        <div className="col-12">
+          {interestQueryReply.loading ? (
+            <div>Loading...</div>
+          ) : (
+            <AwesomeSelect className="form-control w-100" onChange={handleInterestChange} name="Interest" options={interests} />
+          )}
         </div>
+      </div>
+      <br/>
+      <div className="row row-cols-1 row-cols-lg-2">
+        {/* The user connections to other users */}
+        <div className="col">
+          <h5>Profile</h5>
+          <br/>
+          <UserCard user={user} renderGreeting={true} renderEdit={true} />
+        </div>
+        {!matchedUsersQueryResults.loading && matchedUsers.length > 0 && (
+          <div className="col">
+            <h5>Connections</h5>
+                <div className="container d-none d-md-inline contact-grid">
+                  <div className="row">
+                    <div className="col-3 border" >Name</div>
+                    
+                    {/* <div className="col-3 border">Email</div> */}
+                    <div className="col-3 border">Profession</div>
+                    <div className="col-3 border">Org</div>
+                    <div className="col-3 border">Location</div>
+                  </div>
 
-        <div classNameName="row">
-          {/* The user connections to other users */}
-          <div classNameName="col-sm-6">
-            <div classNameName="card">
-              <div className="card-body">
-                <h5 className="card-title">Hi {data.user}!</h5>
-                <p className="card-text">Email:{data.email}</p>
-                <p className="card-text">Password:{data.password}</p>
-                <p className="card-text">Organization:{data.organization}</p>
-                <p className="card-text">Location:{data.location}</p>
-                <p className="card-text">Interests:{}</p>
-                <p className="card-text">Profession:{}</p>
-                <a href="#" className="btn btn-primary">
-                  Edit profile
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* The user connections to other users */}
-          <div className="col-sm-6">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Connections</h5>
+                  {matchedUsers.map((usr) => (
+                    <div key={usr.id} className="row">
+                      <div className="col-3 border"><a href={`mailto:${usr.email}`}> {usr.username}</a></div>
+                      {/* <div className="col-3 border">{usr.email}</div> */}
+                      <div className="col-3 border">{usr.profession.professionOption}</div>
+                      <div className="col-3 border">{usr.organization}</div>
+                      <div className="col-3 border">{usr.location}</div>
+                    </div>
+                  ))}
+                </div>
+                <div className="d-md-none d-inline">
+                  {matchedUsers.map((usr) => (
+                    <UserCard key={usr.id} user={usr} />
+                  ))}
+                </div>
                 <button
                   className="btn btn-block btn-primary"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
-                  Edit Profile
+                  Edit Connections
                 </button>
-              </div>
-            </div>
           </div>
-        </div>
+        )}
+
       </div>
-    </main>
+    </>
   );
 };
 
